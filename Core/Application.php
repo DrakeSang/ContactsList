09d5e0c97c736e80e0a8562413b22991f3f5f516
DrakeSang <emicha@mail.bg>
2019-03-19 21:52:33 +0200

    Adding the application class.There i will keep the controller name, action name and the arguments. Also creating two arrays mappings and instances. The mapping will match the name of the interface and the name of the class which uses it. The instance will keep the the name and the instance created with the keyword new.

--- Core/Application.php
+++ Core/Application.php
@@ -4,5 +4,36 @@ namespace Core;
 
 class Application
 {
+    private  $controllerName;
 
+    private $actionName;
+
+    private $params = [];
+
+    private $mappings = [];
+
+    private $instances = [];
+
+    /**
+     * Application constructor.
+     * @param $controllerName
+     * @param $actionName
+     * @param array $params
+     */
+    public function __construct($controllerName, $actionName, array $params)
+    {
+        $this->controllerName = $controllerName;
+        $this->actionName = $actionName;
+        $this->params = $params;
+    }
+
+    public function addMapping(string $interface, string $className)
+    {
+        $this->mappings[$interface] = $className;
+    }
+
+    public function addInstance(string $interface, $instance)
+    {
+        $this->instances[$interface] = $instance;
+    }
 }
--- index.php
+++ index.php
@@ -5,48 +5,26 @@ spl_autoload_register();
 $entryPointFileName = basename(__FILE__); // getting the base name of the file which is
 // index.php
 
-echo $entryPointFileName;
-echo '<br>';
-
 $self = $_SERVER['PHP_SELF']; // getting the url of index.php which is the name of the project
 // and index.php. In my case it returns ContactsList/index.php
-
-echo $self;
-echo '<br>';
 
 $junk = str_replace($entryPointFileName, '', $self); // I replace index.php with empty
 // string in ContactsList/index.php and returns only ContactsList. I consider this junk,
 // because later i do not need it. I need only the name of the controller and the action.
 
-echo $junk;
-echo '<br>';
-
 $uri = $_SERVER['REQUEST_URI']; // returns the url(for example it returns ContactsList/contact
 //add).That way i can easily replace ContactsList with empty string and my controller name
 //will be ContactController adn teh action will be add.
 
-echo $uri;
-echo '<br>';
-
 $significantUri = str_replace($junk, '', $uri); // Taking only the significant part
 // of the url. For example if the url is ContactsList/contact/add i get only contact and add.
-
-echo $significantUri;
-echo '<br>';
 
 $significantUri = str_replace([$_SERVER['QUERY_STRING'], '?'], '', $significantUri);
 //If user tries to type the url by himself and he uses query string or ? i need to replace
 // them again with empty string to be sure i will use teh correct controller and action.
 
-echo $significantUri;
-echo '<br>';
-
 $uriParts = explode('/', $significantUri); // Splitting the significant url by / and
 // keep them in array. On first index is my controller name on second is action name.
-
-echo '<pre>';
-print_r($uriParts);
-echo '</pre>';
 
 if($uriParts[0] == "" || count($uriParts) == 1){
     $controllerName = "home";
@@ -57,14 +35,6 @@ if($uriParts[0] == "" || count($uriParts) == 1){
     $actionName = array_shift($uriParts);
 } // checking if user is on the main page or he type himself the url and he only writes the
 // the controller name i set the controller name to be home and action name to be index.
-
-echo '<pre>';
-print_r($controllerName);
-echo '</pre>';
-
-echo '<pre>';
-print_r($actionName);
-echo '</pre>';
 
 $arguments = $uriParts; // if the url is contact/show/6 i keep in array the number 6 which
 // maybe it represents the id of the contact, which i need to show to the user.
